e12967d3edcc78b07f05587378bc91303d947e94
TheCodeFather0 <raminmammadov056@gmail.com>
2024-09-24 14:28:52 +0400

    key problem fixed

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -14,8 +14,8 @@ const Header = () => {
             <h3>{username}</h3>
           </div>
           <nav className={styles.nav}>
-            {links.map((link) => {
-              return <a className={styles.links} href={`#${link.toLowerCase()}`} data-to-scrollspy-id={link.toLowerCase()}>{link}</a>;
+            {links.map((link,index) => {
+              return <a key={index} className={styles.links} href={`#${link.toLowerCase()}`} data-to-scrollspy-id={link.toLowerCase()}>{link}</a>;
             })}
           </nav>
         </div>
