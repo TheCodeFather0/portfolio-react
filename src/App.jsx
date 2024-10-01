301008d3a732d3a5fab1e724f8bd941189e3dcde
TheCodeFather0 <raminmammadov056@gmail.com>
2024-10-01 11:20:37 +0400

    about added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Components/Header";
 import Banner from "./Components/Banner";
 import ScrollSpy from "react-ui-scrollspy";
+import About from "./Components/About";
 
 const App = () => {
   return (
     <ScrollSpy>
       <Header />
       <Banner />
+      <About />
     </ScrollSpy>
   );
 };
