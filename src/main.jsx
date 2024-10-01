0668e2424e920407e7937977124486967e2f685f
TheCodeFather0 <raminmammadov056@gmail.com>
2024-10-01 11:19:44 +0400

    bootstrap grid imported

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import './Assets/css/global.css';
-
+import "./Assets/css/global.css";
+import "../node_modules/bootstrap/dist/css/bootstrap-grid.css";
 
 createRoot(document.getElementById("root")).render(<App />);
